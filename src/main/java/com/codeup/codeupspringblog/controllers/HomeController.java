841528d4f5b92e95e64e923e359061dc672adbb9
khinememe kyaw <khinememe.kyaw01@gmail.com>
2023-08-10 16:09:53 -0500

    Completed Rolling Dice and Post exercises

--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,13 +1,35 @@
-package com.codeup.codeupspringblog.controllers;
+package com.codeup.codeupspringblog.controllers;//package com.codeup.codeupspringblog.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//@Controller
+//public class HomeController {
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String welcome(){
+//        return "This is a landing page!";
+//    }
+//}
+
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    @ResponseBody
-    public String welcome(){
-        return "This is a landing page!";
+
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
     }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
 }
--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,37 +1,71 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Post;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
-    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
-    @ResponseBody
-    public String addOne(@PathVariable int number) {
-        return number + " plus one is " + (number + 1) + "!";
-    }
 //    post index page
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String postsIndex() {
-        return "This is the posts index page";
+    @GetMapping( "/posts")
+    public String postsHome(Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
+        posts.add( new Post(1, "Title One", "Body One"));
+        posts.add( new Post(2, "Title Two", "Body Two"));
+        model.addAttribute("posts",posts);
+        return "posts/index";
     }
 //    View an individual post
-    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String postsId(@PathVariable int id){
-        return "Viewing page for id : " + id;
+    @GetMapping( "/posts/{id}")
+    public String postsHome(@PathVariable long id, Model model){
+        Post post = new Post(id, "Title is Here", "Body Goes Here");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 //    view the form for creating a post
-    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @GetMapping( "/posts/create")
     @ResponseBody
-    public String viewCreate(){
-        return "View the form for creating a post";
+    public String postForm(){
+        return "Form goes here";
     }
 //    create a new post
-    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String create() {
-        return "Create a new post";
+    @PostMapping( "/posts/create")
+    public String createPost() {
+        return "Storing the post";
     }
 }
+
+//@Controller
+//public class PostController {
+//    @GetMapping("/posts")
+//    public String postsHome(Model model) {
+//        ArrayList<Post> posts = new ArrayList<>();
+//        posts.add(new Post(1,"Wow!", "Free Brownies in the quad!"));
+//        posts.add(new Post(2,"Uh-oh...", "The brownies betrayed me..."));
+//        model.addAttribute("posts", posts);
+//        return "posts/index";
+//    }
+//
+//    @GetMapping("/posts/{id}")
+//    public String postsHome(@PathVariable long id, Model model) {
+//        Post post = new Post(id, "Test post", "Why do all these posts look the same?");
+//        model.addAttribute("post", post);
+//        return "posts/show";
+//    }
+//
+//    @GetMapping("/posts/create")
+//    @ResponseBody
+//    public String postsForm() {
+//        return "And this is where the form for creating a post would go... IF WE HAD ONE!";
+//    }
+//    @PostMapping("/posts/create")
+//    public void createPost() {
+//        //Something happens here to store a post for later ;)
+//    }
+//}
