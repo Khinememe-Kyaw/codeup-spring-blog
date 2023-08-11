25916b93c727b688e120071d7d770bcb0ffed867
khinememe kyaw <khinememe.kyaw01@gmail.com>
2023-08-11 01:58:26 -0500

    Create post rest website

---
+++ src/main/java/com/codeup/codeupspringblog/Repositories/AdRepository.java
@@ -0,0 +1,9 @@
+//package com.codeup.codeupspringblog.Repositories;
+//
+//import com.codeup.codeupspringblog.models.Ad;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//
+//public interface AdRepository extends JpaRepository<Ad, Long> {
+//
+//}
--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,71 +1,57 @@
-package com.codeup.codeupspringblog.controllers;
-
-import com.codeup.codeupspringblog.models.Post;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-
-@Controller
-public class PostController {
-//    post index page
-    @GetMapping( "/posts")
-    public String postsHome(Model model) {
-        ArrayList<Post> posts = new ArrayList<>();
-        posts.add( new Post(1, "Title One", "Body One"));
-        posts.add( new Post(2, "Title Two", "Body Two"));
-        model.addAttribute("posts",posts);
-        return "posts/index";
-    }
-//    View an individual post
-    @GetMapping( "/posts/{id}")
-    public String postsHome(@PathVariable long id, Model model){
-        Post post = new Post(id, "Title is Here", "Body Goes Here");
-        model.addAttribute("post", post);
-        return "posts/show";
-    }
-//    view the form for creating a post
-    @GetMapping( "/posts/create")
-    @ResponseBody
-    public String postForm(){
-        return "Form goes here";
-    }
-//    create a new post
-    @PostMapping( "/posts/create")
-    public String createPost() {
-        return "Storing the post";
-    }
-}
-
+//package com.codeup.codeupspringblog.controllers;
+//
+//import com.codeup.codeupspringblog.Repositories.AdRepository;
+//import com.codeup.codeupspringblog.Repositories.PostRepository;
+//import com.codeup.codeupspringblog.models.Post;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.ArrayList;
+//
 //@Controller
 //public class PostController {
+//
+//    public PostController(PostRepository postDao) {
+//        this.postDao = postDao;
+//    }
+//
+//    private final PostRepository postDao;
+//
+//
+//    //    post index page
 //    @GetMapping("/posts")
 //    public String postsHome(Model model) {
 //        ArrayList<Post> posts = new ArrayList<>();
-//        posts.add(new Post(1,"Wow!", "Free Brownies in the quad!"));
-//        posts.add(new Post(2,"Uh-oh...", "The brownies betrayed me..."));
+//        posts.add(new Post(1, "Title One", "Body One"));
+//        posts.add(new Post(2, "Title Two", "Body Two"));
 //        model.addAttribute("posts", posts);
 //        return "posts/index";
 //    }
 //
+//    //    View an individual post
 //    @GetMapping("/posts/{id}")
 //    public String postsHome(@PathVariable long id, Model model) {
-//        Post post = new Post(id, "Test post", "Why do all these posts look the same?");
+//        Post post = new Post(id, "Title is Here", "Body Goes Here");
 //        model.addAttribute("post", post);
 //        return "posts/show";
 //    }
 //
+//    //    view the form for creating a post
 //    @GetMapping("/posts/create")
-//    @ResponseBody
-//    public String postsForm() {
-//        return "And this is where the form for creating a post would go... IF WE HAD ONE!";
+//    public String postsForm(Model model) {
+//        model.addAttribute("post", new Post());
+//        model.addAttribute("heading", "Create new post.");
+//        return "posts/create";
 //    }
-//    @PostMapping("/posts/create")
-//    public void createPost() {
-//        //Something happens here to store a post for later ;)
+//
+//    @PostMapping("/posts/save")
+//    public String createPost(@ModelAttribute Post post) {
+//        Post origPost = postDao.findPostById(post.getId());
+//        if (origPost == null) {
+//            postDao.save(post);
+//        }
+//        return "redirect:/posts";
 //    }
+//
 //}
